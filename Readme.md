d11ccddb21c34d1e7e26642193b249e774a0382d
07amansaini8684 <mr.amannobody@gmail.com>
2024-09-16 15:13:29 +0530

    regiseter user and bug

--- Readme.md
+++ Readme.md
@@ -1,1 +1,14 @@
 this project is about making youtube like platform , it is mainly backed project bruuuuhhhhhhhhhh
+
+
+// register steps 
+get user details form forntend
+validation - not empty
+check if user already esists : username , email
+check for images, check for avatar
+upload them to cloudinary, avatar
+create user object - create entry in db 
+remove password and refresh token fileld from response 
+check for user creation 
+return response
+
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,64 @@
-import {asynchandler} from "../utils/asynchandler.js"
+import { asynchandler } from "../utils/asynchandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import {uploadCloudinary} from "../utils/cloudinary.js"
 
-const registerUser = asynchandler(async (req,res)=>{
-    res.status(200).json({
-        message:"chel reha hai"
-    })
-})
- 
-export {registerUser}
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const registerUser = asynchandler(async (req, res) => {
+    const { fullname, username, email, password } = req.body;
+    console.log(fullname, username, password, email);
+    if (
+        [fullname, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "full information is required");
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }],
+    });
+    if (existedUser) {
+        throw new ApiError(409, "username or email already exists");
+    }
+
+    const avatarFilepath = req.files?.avatar[0]?.path;
+    const coverImageFilepath = req.files?.coverImage[0]?.path
+
+    if (!avatarFilepath) {
+        throw new ApiError(400, "avatar is required");
+    }
+
+   const avatar = await uploadCloudinary(avatarFilepath)
+   const coverImage = await uploadCloudinary(coverImageFilepath)
+
+   if(!avatar){
+    throw new ApiError(400, "avatar upload failed")
+   }
+
+   const creatUser = await User.create({
+    fullname,
+    username : username.toLowerCase(),
+    email,
+    password,
+    avatar : avatar.url,
+    coverImage : coverImage?.url || "",
+   })
+
+  const createdUser =  await User.findById(creatUser._id).select(
+    "-password -refreshToken",
+
+   )
+   if(!createdUser){
+    throw new ApiError(500, "Something went wrong , user not found")
+   }
+
+   return res.status(201).json(
+    new ApiRespons(200, createdUser, "User regisetered Successfully")
+   )
+
+   
+
+
+});
+
+export { registerUser };
