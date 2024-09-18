fb17a9329e306b777ea3d1aadd8190c3c86f671d
07amansaini8684 <mr.amannobody@gmail.com>
2024-09-18 17:43:39 +0530

    setting router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  updateUserDetails,
 } from "../controllers/user.controller.js";
 
 import upload from "../middlewares/multerMiddleware.js";
@@ -30,5 +37,20 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyjwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyjwt, changeCurrentPassword);
+router.route("/current-user").get(verifyjwt, getCurrentUser);
+router.route("/update-details").patch(verifyjwt, updateUserDetails);
+router
+  .route("/avatar")
+  .patch(verifyjwt, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyjwt, upload.single("coverImage"), updateUserCoverImage);
+
+// getting form params
+router.route("/channel/:username").get(verifyjwt, getUserChannelProfile);
+//
+router.route("/history").get(verifyjwt, getWatchHistory);
 
 export default router;
