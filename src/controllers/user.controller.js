356749faf43e8cce83c7a000abc179cb20461b44
07amansaini8684 <mr.amannobody@gmail.com>
2024-09-18 17:32:57 +0530

    watchHistory piplines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
 
 const registerUser = asynchandler(async (req, res) => {
   const { fullname, username, email, password } = req.body;
@@ -364,6 +365,53 @@ const getUserChannelProfile = asynchandler(async (req, res) => {
 });
 
 
+const getWatchHistory = asynchandler(async(req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from: "users",
+              localField:"owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullname:1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first :"$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res.status(200).json(new ApiResponse(200, user[0].watchHistory,"Watch History fetched Successfully"))
+})
+
+
 export {
   registerUser,
   loginUser,
@@ -374,6 +422,9 @@ export {
   updateUserDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+
 };
 
 // Todos
